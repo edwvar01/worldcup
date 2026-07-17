da240e58fe39e3c2fef41ebdf2e3a9a5c1cac8cb
unknown <edwin jijo>
2026-07-17 22:04:42 +0530

    stream added

--- app.js
+++ app.js
@@ -2055,3 +2055,30 @@ async function joinStream() {
         }
     });
 }
+
+function toggleFullScreen(elementId) {
+    const elem = document.getElementById(elementId);
+    if (!elem) return;
+    
+    if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+        } else if (elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) {
+            elem.webkitRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+}
